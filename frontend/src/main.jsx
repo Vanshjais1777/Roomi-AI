56f50cb3f2a11c1f9a62f887823a6eb291e97ad3
vansh jaiswal <vanshjais1777@gmail.com>
2024-11-08 01:17:33 +0530

    main.jsx import corrected

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BackendUrlProvider } from './context/BackendUrlContext.jsx'
-import { AuthProvider } from './context/authContext.jsx'
+import { AuthProvider } from './context/AuthContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BackendUrlProvider>
